f7a00a0187b60fa897b5900cdc628590527dd61c
Boots2291 <buteramv@gmail.com>
2017-06-29 18:24:56 -0600

    First commit

---
+++ lib/binary_search_tree.rb
@@ -0,0 +1,49 @@
+class BinarySearchTree
+  attr_accessor :root
+
+  def initialize
+    @root == nil
+  end
+
+  def insert
+
+  end
+
+  def include?
+
+  end
+
+  def depth_of
+
+  end
+
+  def max
+
+  end
+
+  def min
+
+  end
+
+  def sort
+
+  end
+
+  def load
+
+  end
+
+  def health
+
+  end
+
+  # Extensions
+  # def leaves
+  #
+  # end
+  #
+  # def height
+  #
+  # end
+
+end
---
+++ lib/node.rb
@@ -0,0 +1,8 @@
+class Node
+  # needs to store title and score, left node, right node, and depth
+  def initialize(score, title)
+    @title_and_score
+    @left = nil
+    @right = nil
+    @depth =
+end
