c2f147d6c2bae21c7af2e119cb7a41b9afcb75ee
Boots2291 <buteramv@gmail.com>
2017-06-29 18:49:21 -0600

    Set up initialize function in node.rb

--- lib/node.rb
+++ lib/node.rb
@@ -1,8 +1,8 @@
 class Node
   # needs to store title and score, left node, right node, and depth
   def initialize(score, title)
-    @title_and_score
+    @title_and_score = {title => score} # => creates a hash
     @left = nil
     @right = nil
-    @depth =
+    @depth = 0
 end
